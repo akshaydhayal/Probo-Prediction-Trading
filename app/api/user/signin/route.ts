80a37a2b13726d3577383ee917594345b1e2a3f9
akshaydhayal <akshaydhayalkd99@gmail.com>
2025-01-30 08:12:26 +0530

    added user signup,signin endpoints

---
+++ app/api/user/signin/route.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import {z} from "zod";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const prismaClient=new PrismaClient();
+
+const signinSchema=z.object({
+    email:z.string().email(),
+    password:z.string().min(5).max(30),
+})
+
+export async function POST(req:NextRequest){
+    try{
+        const body=await req.json();
+        const parsedResponse=signinSchema.safeParse(body);
+        if(!parsedResponse.success){
+            return NextResponse.json({msg:parsedResponse.error},{status:401});
+        }
+        const {email,password}=parsedResponse.data;
+        const user=await prismaClient.user.findFirst({where:{email}});
+        if(!user){
+            return NextResponse.json({msg:"User not found"},{status:401});
+        }
+        const isPasswordMatched=await bcrypt.compare(password,user.password);
+        if(!isPasswordMatched){
+            return NextResponse.json({msg:"Invalid password"},{status:401});
+        }
+        if(!process.env.JWT_SECRET){
+            return NextResponse.json({msg:"JWT secret is not defined"},{status:401});
+        }
+        const jwtToken=jwt.sign({userId:user.id},process.env.JWT_SECRET);
+        return NextResponse.json({msg:"User signed in!!",jwtToken},{status:201});
+    }catch(e){
+        console.log(e);
+        return NextResponse.json({msg:"Internal server error",error:e},{status:501});
+    }
+}
--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,6 +1,31 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import {z} from "zod";
+import bcrypt from "bcrypt";
 
-export function POST(re:NextRequest){
-    
-    return NextResponse.json({msg:"Hello"},{status:200});
+const prismaClient=new PrismaClient();
+
+const signupSchema=z.object({
+    name:z.string().min(5).max(30),
+    email:z.string().email(),
+    password:z.string().min(5).max(30),
+})
+
+export async function POST(req:NextRequest){
+    try{
+        const body=await req.json();
+        const parsedResponse=signupSchema.safeParse(body);
+        if(!parsedResponse.success){
+            return NextResponse.json({msg:parsedResponse.error},{status:401});
+        }
+        const {name,email,password}=parsedResponse.data;
+        const hashedPassword=await bcrypt.hash(password,10);
+        await prismaClient.user.create({
+            data:{email,name,password:hashedPassword}
+        })
+        return NextResponse.json({msg:"User signed up!!"},{status:201});
+    }catch(e){
+        console.log(e);
+        return NextResponse.json({msg:"Internal server error",error:e},{status:501});
+    }
 }
