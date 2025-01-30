8a36096f74a74bce0f9346b8ca28b8f0d50f399a
akshaydhayal <akshaydhayalkd99@gmail.com>
2025-01-30 20:00:09 +0530

    add admin role to decalre results

--- app/api/event/[eventId]/route.ts
+++ app/api/event/[eventId]/route.ts
@@ -106,13 +106,24 @@ export async function PUT(req: NextRequest, { params }: { params: { eventId: str
     const body = await req.json();
     const { eventId } = await params;
     const parsedResponse = z.object({ result: z.enum(["YES", "NO", "PENDING"]) }).safeParse(body);
-    if (!parsedResponse.success) {
-      return NextResponse.json({ msg: parsedResponse.error }, { status: 400 });
-    }
+    if (!parsedResponse.success)  return NextResponse.json({ msg: parsedResponse.error }, { status: 400 });
+    
     const { result } = parsedResponse.data;
-    if (result == "PENDING") {
-      return NextResponse.json({ msg: "Event result is pending" }, { status: 400 });
+    if (result == "PENDING")  return NextResponse.json({ msg: "Result cannot be PENDING" }, { status: 400 });
+
+    //to check if user has adminrole to decalre result
+    const token=req.headers.get("token");
+    if(!token) return NextResponse.json({msg:"JWT not provided!!"},{status:401});
+    if(!process.env.JWT_SECRET) return NextResponse.json({msg:"JWT SECRET not provided!!"},{status:401});
+    const decodedToken=jwt.verify(token,process.env.JWT_SECRET);
+    const userId=(decodedToken as JwtPayload).userId as number;
+    const user=await prismaClient.user.findUnique({where:{id:userId}});
+    if(!user) return NextResponse.json({msg:"User not found!!"},{status:401});
+    if(user.role!='ADMIN'){
+        return NextResponse.json({msg:"You are not authorized to declare result!!"},{status:401});
     }
+
+    //getting the event
     const event = await prismaClient.event.findUnique({
       where: { id: Number(eventId) },
       include: { bettings: true },
@@ -124,10 +135,10 @@ export async function PUT(req: NextRequest, { params }: { params: { eventId: str
       return NextResponse.json({ msg: "Event result already set" }, { status: 400 });
     }
 
+    //transaction- evern result update and even money distribution
     const allWinBettings = event.bettings.filter((b) => b.prediction == result);
     const winOdds: number = event.totalBetting / (result == "YES" ? event.yesBetting : event.noBetting);
 
-    //transaction- evern result update and even money distribution
     const txs = allWinBettings.map((bet) => {
       return prismaClient.user.update({
         where: { id: bet.userId },
